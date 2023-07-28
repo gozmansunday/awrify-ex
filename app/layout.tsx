6b0abdf3fb077ce2fa0c199a15c78a6aa6caccef
Goziem Sunday <thegzmn@gmail.com>
2023-07-28 20:41:36 +0100

    Init Next.js app

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import { ReactNode } from 'react'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Awrific Music',
+}
+
+interface Props {
+  children: ReactNode;
+}
+
+const RootLayout = ({children}: Props) => {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {children}
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout;
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+
+const Home = () => {
+  return (
+    <main>
+
+    </main>
+  )
+};
+
+export default Home;
