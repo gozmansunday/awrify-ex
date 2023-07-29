6afe9aae1f98d82e69d19d77ea1df7414e74d44c
Goziem Sunday <thegzmn@gmail.com>
2023-07-29 11:08:45 +0100

    Update app

---
+++ lib/fontConfig.ts
@@ -0,0 +1,25 @@
+import localFont from 'next/font/local';
+
+export const clash = localFont({
+  src: '../public/fonts/Clash.woff2',
+  display: 'swap',
+  weight: '400',
+  style: 'normal',
+  variable: '--font-clash',
+});
+
+export const mona = localFont({
+  src: '../public/fonts/Mona.woff2',
+  display: 'swap',
+  weight: '400',
+  style: 'normal',
+  variable: '--font-mona',
+});
+
+export const nohemi = localFont({
+  src: '../public/fonts/Nohemi.woff2',
+  display: 'swap',
+  weight: '400',
+  style: 'normal',
+  variable: '--font-nohemi',
+});
