8108ed91529a850bc126d79035dd1216e54faaf1
Goziem Sunday <thegzmn@gmail.com>
2023-08-05 17:37:14 +0100

    Update app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,9 @@ import "./globals.css";
 import { clash, hubot, manrope, mona, neo, nohemi } from "@/lib/fontConfig";
 import Wrapper from "@/components/Wrapper";
 import SupabaseProvider from "@/providers/SupabaseProvider";
+import { MyUserContextProvider } from "@/hooks/useUser";
+import UserProvider from "@/providers/UserProvider";
+import ModalProvider from "@/providers/ModalProvider";
 
 export const metadata: Metadata = {
   title: "Awrify",
@@ -22,9 +25,12 @@ const RootLayout = ({ children }: Props) => {
     <html lang="en" className={`${fontVariables}`}>
       <body className="font-hubot bg-darkest">
         <SupabaseProvider>
-          <Wrapper>
-            {children}
-          </Wrapper>
+          <UserProvider>
+            <ModalProvider />
+            <Wrapper>
+              {children}
+            </Wrapper>
+          </UserProvider>
         </SupabaseProvider>
       </body>
     </html>
