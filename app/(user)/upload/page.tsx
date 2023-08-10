cee9cc316047fd8407c7df9a37dd348599bdc2c2
Goziem Sunday <thegzmn@gmail.com>
2023-08-10 15:42:21 +0100

    Implement 'add song to library' feature

--- app/(user)/upload/page.tsx
+++ app/(user)/upload/page.tsx
@@ -1,19 +1,26 @@
 "use client";
 
+import { useState } from "react";
 import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
+import { v4 as uuidv4 } from 'uuid';
 
 // Local imports
 import UserPageContent from "@/components/UserPageContent";
 import { useRouter } from "next/navigation";
 import { Input } from "@/components/ui/input";
-import { useState } from "react";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
+import { ToastAction } from "@/components/ui/toast";
+import { useToast } from "@/components/ui/use-toast";
+import { useUserDataStore } from "@/hooks/useStore";
+import supabase from "@/config/supabaseClient";
 
 const UploadSongPage = () => {
-  const [isLoading, setIsLoading] = useState<boolean>();
+  const [isLoading, setIsLoading] = useState(false);
+  const { userData, setUserData } = useUserDataStore();
 
   const router = useRouter();
+  const { toast } = useToast();
   const { register, handleSubmit, reset } = useForm<FieldValues>({
     defaultValues: {
       title: "",
@@ -31,7 +38,101 @@ const UploadSongPage = () => {
   };
 
   const handleSongSubmit: SubmitHandler<FieldValues> = async (values) => {
-    // Submit to supabase
+    try {
+      setIsLoading(true);
+
+      const songFile = values.song?.[0];
+      const imageFile = values.image?.[0];
+
+      if (!songFile || !imageFile || !userData) {
+        toast({
+          variant: "destructive",
+          title: "Uh oh! Something went wrong.",
+          description: "There are some fields missing.",
+          action: <ToastAction altText="Close">Close</ToastAction>
+        });
+        return;
+      }
+
+      const uniqueID = uuidv4();
+
+      // Upload song
+      const { data: songData, error: songError } = await supabase
+        .storage
+        .from("songs")
+        .upload(`song-${values.title}-${uniqueID}`, songFile, {
+          cacheControl: "3600",
+          upsert: false
+        });
+
+      if (songError) {
+        setIsLoading(false);
+        return toast({
+          variant: "destructive",
+          title: "Uh oh! Something went wrong.",
+          description: "Failed song upload.",
+          action: <ToastAction altText="Close">Close</ToastAction>
+        });
+      }
+
+      // Upload image
+      const { data: imageData, error: imageError } = await supabase
+        .storage
+        .from("images")
+        .upload(`image-${values.title}-${uniqueID}`, imageFile, {
+          cacheControl: "3600",
+          upsert: false
+        });
+
+      if (imageError) {
+        setIsLoading(false);
+        return toast({
+          variant: "destructive",
+          title: "Uh oh! Something went wrong.",
+          description: "Failed image upload.",
+          action: <ToastAction altText="Close">Close</ToastAction>
+        });
+      }
+
+      // Insert to the supabase database
+      const { error: supabaseError } = await supabase
+        .from("songs")
+        .insert({
+          user_id: userData.user.id,
+          title: values.title,
+          author: values.title,
+          image_path: imageData.path,
+          song_path: songData.path,
+        });
+
+      if (supabaseError) {
+        setIsLoading(false);
+        return toast({
+          variant: "destructive",
+          title: "Uh oh! Something went wrong.",
+          description: `${supabaseError.message}`,
+          action: <ToastAction altText="Close">Close</ToastAction>
+        });
+      }
+
+      router.push("/");
+      reset();
+      setIsLoading(false);
+      toast({
+        title: "Song created!",
+        description: "Your song has been created.",
+        action: <ToastAction altText="Close">Close</ToastAction>
+      });
+    } catch (error) {
+      toast({
+        variant: "destructive",
+        title: "Uh oh! Something went wrong.",
+        description: "There was a problem with your request.",
+        action: <ToastAction altText="Close">Close</ToastAction>
+      });
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
