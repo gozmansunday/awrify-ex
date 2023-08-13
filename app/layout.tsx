0779e9d004e9607b960804fca1675e44abc5913e
Goziem Sunday <thegzmn@gmail.com>
2023-08-13 03:26:05 +0100

    Provide user data and song data to app

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { clash, hubot, manrope, mona, neo, nohemi } from "@/lib/fontConfig";
 import { Toaster } from "@/components/ui/toaster";
+import UserDataProvider from "@/providers/UserDataProvider";
+import SongsDataProvider from "@/providers/SongsDataProvider";
 
 export const metadata: Metadata = {
   title: "Awrify",
@@ -20,8 +22,12 @@ const RootLayout = ({ children }: Props) => {
   return (
     <html lang="en" className={`${fontVariables}`}>
       <body className="font-hubot bg-darkest min-h-[100dvh]">
-        <Toaster />
-        {children}
+        <UserDataProvider>
+          <SongsDataProvider>
+            <Toaster />
+            {children}
+          </SongsDataProvider>
+        </UserDataProvider>
     </body>
     </html>
   )
